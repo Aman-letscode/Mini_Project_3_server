87c4e4b2c971fe4fdc02e8a8187fa532224c8b84
pranavhingankar <hingankarpranav123@gmail.com>
2023-04-25 17:52:03 +0530

    added something

--- models/case.js
+++ models/case.js
@@ -1,29 +1,37 @@
 const mongoose = require('mongoose')
+const User = require("../models/users");
 
-
-const caseSchema = new mongoose.Schema({
+const caseSchema = new mongoose.Schema(
+  {
     case_id: {
-        type:String,
-        required:[true,"can't be blank"],
-        trim: true
+      type: String,
+      required: [true, "can't be blank"],
+      trim: true,
     },
-    status:{
-        type: String,
-        enum: ['closed','active']
+    status: {
+      type: String,
+      enum: ["closed", "active"],
     },
-    
+
     user_id: {
-        type:Array,
-        required: false,
-        default: null,
+      type: Array,
+      required: false,
+      default: null,
     },
-    
-    last_active:{
-        type: Date,
-        required:false
-    }
-},
-{timestamps:true})
+
+    last_active: {
+      type: Date,
+      required: false,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.Array,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const caseModel =  mongoose.model('case',caseSchema);
 
